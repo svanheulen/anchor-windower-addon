2bf94cfba4d1bef714638a2ac6054261362dcfa3
svanheulen <svanheulen@gmail.com>
2014-09-10 22:35:08 -0400

    fixed incoming chunk callback return value, added license

--- anchor.lua
+++ anchor.lua
@@ -1,3 +1,20 @@
+--[[
+Copyright 2014 Seth VanHeulen
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
+General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+--]]
+
 _addon.name = 'anchor'
 _addon.version = '1.0.0'
 _addon.command = 'anchor'
@@ -44,6 +61,7 @@ function check_incoming_chunk(id, original, modified, injected, blocked)
                 end
                 startbit = startbit + 123
             end
+            return new
         end
     end
 end
